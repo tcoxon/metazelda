1cc9a46fdfc89bee1ebb9ea63d0f1671866bab49
Tom Coxon <tom.c.coxon@googlemail.com>
2014-02-25 21:03:11 +0000

    Fix a warning.

--- src/net/bytten/metazelda/util/CoordsMap.java
+++ src/net/bytten/metazelda/util/CoordsMap.java
@@ -3,8 +3,9 @@ package net.bytten.metazelda.util;
 import java.util.TreeMap;
 
 public class CoordsMap<V> extends TreeMap<Coords, V> {
+	private static final long serialVersionUID = 1L;
 
-    public V get(int x, int y) {
+	public V get(int x, int y) {
         return get(new Coords(x,y));
     }
     
